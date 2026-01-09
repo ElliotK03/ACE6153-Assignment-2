a0603f222a0ad2a3f4f18cdd9b1be90ed9eb262e
ElliotK03 <164868526+ElliotK03@users.noreply.github.com>
2026-01-09 16:39:42 +0800

    Revised UI code; Added helper functions

--- main.c
+++ main.c
@@ -7,11 +7,20 @@
 // 6, 3, 5, 2, 5, 6, 3, 5, 9, 0, 0, 1, 2, 4, 7
 
 #include <stdio.h>
+#include <string.h>
 
 #define CACHE_SIZE 3
+// Cache is modeled using an array,
+// index 0 is the start (top) of the cache
 
 int input[] = {6, 3, 5, 2, 5, 6, 3, 5, 9, 0, 0, 1, 2, 4, 7};
 
+// helper functions
+void shiftCacheContentsDown(int []);
+void floatCacheContent(int [], int);
+int findIndex(int cache[], int value);
+
+// page replacement algorithms
 void inputFIFO(int input, int cache[], int _counter[]) {};
 
 void inputLFU(int input, int cache[], int counter[]) {};
@@ -22,12 +31,15 @@ int main() {
 
   void (*processInput)(int inputValue, int cache[], int counter[]);
 
+  // assume the cache is holding pointers instead of ints,
+  // negative values represent null
   int cache[CACHE_SIZE] = {1, -2, -4};
   int counter[CACHE_SIZE] = {1, 20, 0};
 
   int inputSize = sizeof(input) / sizeof(input[0]);
 
   int algoSelection = 0;
+  char algoName[5];
 
   printf("Select Page Replacement Algorithm: \n");
   printf("1. FIFO \n");
@@ -36,30 +48,30 @@ int main() {
 
   printf("Select number: ");
   scanf("%d", &algoSelection);
-
-  printf("The algorithm you selected is");
-
+  
   switch (algoSelection) {
   case 1: {
     processInput = &inputFIFO;
-    printf(" FIFO\n\n");
+    strcpy(algoName, "FIFO");
     break;
   }
   case 2: {
     processInput = &inputLFU;
-    printf(" LFU\n\n");
+    strcpy(algoName, "LFU");
     break;
   }
   case 3: {
     processInput = &inputLRU;
-    printf(" LRU\n\n");
+    strcpy(algoName, "LRU");
     break;
   }
-  default:
+  default: {
+    printf("Invalid selection, program will exit now.");
     return 1;
   }
+  }
 
-  // printf("%d", algoSelection);
+  printf("The algorithm you selected is %s\n\n", algoName);
 
   for (int i = 0; i < inputSize; i++) {
     // process the input
@@ -103,4 +115,38 @@ int main() {
   }
 
   return 0;
+}
+
+// shifts the entire cache down, removes the last item from cache
+void shiftCacheContentsDown(int cache[]) {
+  for (int i = CACHE_SIZE - 1; i > 0; i--) {
+    cache[i] = cache[i-1];
+  }
+  
+  cache[0] = -1;
+}
+
+// shifts the content in cache[index] to the top of the cache
+void floatCacheContent(int cache[], int index) {
+  if (index >= CACHE_SIZE || index < 0){ 
+    printf("Error! Invalid index %d", index);
+    return;
+  }
+  
+  int temp, i;
+
+  for (i=index; i > 0; i--) {
+    temp = cache[i-1];
+    cache[i-1] = cache[i];
+    cache[i] = temp;
+  }
+}
+
+int findIndex(int cache[], int value) {
+
+  for (int i = 0; i < CACHE_SIZE; i++) {
+    if (cache[i] == value) return i;
+  }
+
+  return -1;
 }
